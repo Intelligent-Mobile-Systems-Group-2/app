bd806ab68e863da0b38c43c223658b44561eddf2
Erik Granlund <kireerik2008@hotmail.com>
2022-04-20 14:51:31 +0200

    clear git cach. added gitignore

--- lib/src/app/view-model/bluetooth_view_model.dart
+++ lib/src/app/view-model/bluetooth_view_model.dart
@@ -11,3 +11,62 @@ import 'package:ims/src/app/bluetooth/controllers/robot_controller.dart';
 import 'package:ims/src/app/bluetooth/interactors/robot_interactor.dart';
 import 'package:stacked/stacked.dart';
 import '../bluetooth/bluetooth_device_manager.dart';
+
+class BluetoothViewModel extends BaseViewModel {
+  BluetoothDiscoverManager _bluetoothDiscoverManager;
+  BluetoothDeviceManager _bluetoothDeviceManager;
+  RobotInteractor _robotInteractor;
+  RobotController _robotController;
+
+  BluetoothViewModel() {
+    _bluetoothDiscoverManager = GetIt.I<BluetoothDiscoverManager>();
+    _bluetoothDeviceManager = GetIt.I<BluetoothDeviceManager>();
+    _robotInteractor = GetIt.I<RobotInteractor>();
+    _robotController = GetIt.I<RobotController>();
+  }
+
+  Stream<List<BluetoothDevice>> get devices => _bluetoothDeviceManager.devices;
+
+  Future<void> startScan({ScanMode scanMode = ScanMode.balanced}) async {
+    await _bluetoothDiscoverManager.startScan(timeout: Duration(seconds: 5), scanMode: scanMode);
+  }
+
+  Future<void> stopScan() async {
+    await _bluetoothDiscoverManager.stopScan();
+  }
+
+  Future<void> connect(BluetoothDevice device) async {
+    await _bluetoothDeviceManager.connect(device);
+  }
+
+  Future<void> disconnect() async {
+    await _bluetoothDeviceManager.disconnect();
+  }
+
+  Future<void> write(Guid uuid, List<int> value) async {
+    await _robotInteractor.write(uuid, value);
+  }
+
+  Future<List<int>> read(Guid uuid) async {
+    return await _robotInteractor.read(uuid);
+  }
+
+  Future<bool> setNotify(Guid uuid, bool state) async {
+    return await _robotInteractor.setNotify(uuid, state);
+  }
+
+  Stream<List<int>> value(Guid uuid) {
+    return _robotInteractor.value(uuid);
+  }
+
+  Future<void> disconnectAll() async {
+    await _bluetoothDeviceManager.disconnectAll();
+  }
+
+  Future<void> connectAll() async {
+    await _bluetoothDeviceManager.connectAll();
+  }
+
+  Future
+  
+}
