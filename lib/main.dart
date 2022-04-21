246d6ddb9aca6dbc62f5da07759f99712e9b6b00
joakimchan <chjo1695@student.ju.se>
2022-04-21 15:30:27 +0200

    finish joystick

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:control_pad/views/joystick_view.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
-void main() {
-  runApp(ExampleApp());
+Future<void> main() async {
+  runApp(MainScreen());
 }
 
-class ExampleApp extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Control Pad Ee',
-      home: HomePage(),
+      title: "joypad with BLE",
+      debugShowCheckedModeBanner: false,
+      home: JoyPad(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class JoyPad extends StatefulWidget {
+  @override
+  _JoyPadState createState() => _JoyPadState();
+}
+
+class _JoyPadState extends State<JoyPad> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Control Pad Ee'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: AppBar(title: Text("nbar")),
+      body: Container(
+          child: Row(
         children: <Widget>[
-          SizedBox(height: 5),
-          JoystickView(size: 200),
+          JoystickView(
+            onDirectionChanged: (double degree, double distance) {
+              double data = degreeTranslater(degree);
+              print(
+                  "degrees: ${degree.toStringAsFixed(2)} distance: ${distance.toStringAsFixed(2)} sin: ${data.toStringAsFixed((2))} ");
+            },
+          ),
         ],
-      ),
+      )),
     );
   }
+}
+
+double degreeTranslater(double degree) {
+  if (degree > 0 && degree < 90) {
+    return (degree / 90);
+  }
+
+  if (degree > 90 && degree < 180) {
+    return ((90 - (degree - 90)) / 90);
+  }
+
+  if (degree > 180 && degree < 270) {
+    return -((degree - 180) / 90);
+  }
+
+  if (degree > 270 && degree < 360) {
+    return -((270 - (degree - 90)) / 90);
+  }
+
+  return 0;
 }
