747dff8202234e62e5bfa81aad7d6e857933db27
joakimchan <chjo1695@student.ju.se>
2022-04-27 11:28:39 +0200

    update of bluetooth view

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:control_pad/views/joystick_view.dart';
+import 'package:get_it/get_it.dart';
+import 'package:get/get.dart';
 
-import 'package:int_app/src/app/view/bluetooth-view/bluetoothBody.dart';
+import 'package:int_app/src/app/view-model/bluetooth_view_model.dart';
+import 'package:int_app/src/app/view/bluetooth-view/bluetooth_view.dart';
+import 'src/app/bluetooth/bluetooth_device_manager.dart';
+import 'src/app/bluetooth/interactors/robot_interactor.dart';
+import 'src/app/bluetooth/controllers/robot_controller.dart';
+import 'src/app/bluetooth/bluetooth_discover_manager.dart';
+
+GetIt getIt = GetIt.instance;
 
 void main() {
+  getIt.registerSingleton(BluetoothDiscoverManager());
+  getIt.registerSingleton(
+      BluetoothDeviceManager<RobotInteractor, RobotController>());
   runApp(ExampleApp());
 }
 
@@ -11,7 +23,7 @@ class ExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Control Pad Ee', home: FindDevicesScreen(),
+      title: 'Control Pad', home: BluetoothView(),
 //      home: HomePage(),
     );
   }
---
+++ lib/src/app/view/bluetooth-view/device_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:int_app/src/app/view-model/bluetooth_view_model.dart';
+import 'package:int_app/src/app/view/bluetooth-view/device_card.dart';
+import 'package:stacked/stacked.dart';
+
+class DeviceList extends ViewModelBuilderWidget<BluetoothViewModel> {
+  final BluetoothViewModel _viewModel;
+
+  const DeviceList(this._viewModel);
+
+  @override
+  bool get disposeViewModel => false;
+
+  @override
+  Widget builder(
+      BuildContext context, BluetoothViewModel viewModel, Widget? child) {
+    return StreamBuilder<List<ScanResult>>(
+      stream: viewModel.scanResults,
+      builder: (context, AsyncSnapshot<List<ScanResult>> snapshot) {
+        return ListView.builder(
+          itemCount: snapshot.data?.length ?? 0,
+          itemBuilder: (_, index) {
+            return DeviceCard(viewModel, snapshot.data![index].device);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  BluetoothViewModel viewModelBuilder(BuildContext context) {
+    return this._viewModel;
+  }
+}
