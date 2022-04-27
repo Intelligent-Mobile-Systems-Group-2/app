90da273faf70a2332150b3996da748b292a02f83
joakimchan <chjo1695@student.ju.se>
2022-04-27 13:04:58 +0200

    extracted joystick view from main into separte folder

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:control_pad/views/joystick_view.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
 
+import 'package:int_app/src/app/view/home/joystick.dart';
+
 Future<void> main() async {
   runApp(MainScreen());
 }
@@ -11,58 +13,9 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "joypad with BLE",
-      debugShowCheckedModeBanner: false,
+      title: 'main',
+//      home: BluetoothView(),
       home: JoyPad(),
     );
   }
-}
-
-class JoyPad extends StatefulWidget {
-  @override
-  _JoyPadState createState() => _JoyPadState();
-}
-
-class _JoyPadState extends State<JoyPad> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("nbar")),
-      body: Container(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          JoystickView(
-            onDirectionChanged: (double degree, double distance) {
-              double data = degreeTranslater(degree);
-              print(
-                  "degrees: ${degree.toStringAsFixed(2)} distance: ${distance.toStringAsFixed(2)} sin: ${data.toStringAsFixed((2))} ");
-            },
-          ),
-        ],
-      )),
-    );
-  }
-}
-
-double degreeTranslater(double degree) {
-  if (degree > 0 && degree < 90) {
-    return (degree / 90);
-  }
-
-  if (degree > 90 && degree < 180) {
-    return ((90 - (degree - 90)) / 90);
-  }
-
-  if (degree > 180 && degree < 270) {
-    return -((degree - 180) / 90);
-  }
-
-  if (degree > 270 && degree < 360) {
-    return -((270 - (degree - 90)) / 90);
-  }
-
-  return 0;
 }
