933c8345bdb684b50b1727f203d6f51b088136f9
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2022-05-09 16:06:30 +0200

    Points scaled to adjust within box

--- lib/src/app/view/Canvas.dart
+++ lib/src/app/view/Canvas.dart
@@ -2,6 +2,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class MowerMap extends CustomPainter {
   final List<DrawModel> pointsList;
@@ -10,18 +12,28 @@ class MowerMap extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    print(size);
+
     canvas.translate((size.width / 2), (size.height / 2));
     final paint = Paint()
       ..color = Colors.red
       ..strokeWidth = 4.0
       ..strokeCap = StrokeCap.round;
 
-    for (int i = 0; i < (pointsList.length - 1); i++) {
-      if (pointsList[i] != null && pointsList[i + 1] != null) {
-        canvas.drawLine(pointsList[i].offset, pointsList[i + 1].offset, paint);
-      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
+    List<DrawModel> pointsListScaled = scalePoints(size, pointsList);
+
+    for (int i = 0; i < pointsList.length; i++) {
+      print(
+          'Point: ${pointsList[i].offset}, Point scaled: ${pointsListScaled[i].offset}');
+    }
+    for (int i = 0; i < (pointsListScaled.length - 1); i++) {
+      if (pointsListScaled[i] != null && pointsListScaled[i + 1] != null) {
+        canvas.drawLine(
+            pointsListScaled[i].offset, pointsListScaled[i + 1].offset, paint);
+      } else if (pointsListScaled[i] != null &&
+          pointsListScaled[i + 1] == null) {
         List<Offset> offsetList = [];
-        offsetList.add(pointsList[i].offset);
+        offsetList.add(pointsListScaled[i].offset);
         canvas.drawPoints(PointMode.points, offsetList, paint);
       }
     }
@@ -37,4 +49,33 @@ class MowerMap extends CustomPainter {
 class DrawModel {
   final Offset offset;
   DrawModel(this.offset);
+}
+
+scalePoints(Size windowSize, List<DrawModel> pointsList) {
+  final scaleVal = getScaleValue(windowSize, pointsList);
+  final points = pointsList.map((e) => (e.offset * scaleVal) / 2);
+
+  List<DrawModel> pointsListScaled = [];
+  for (var point in points) {
+    pointsListScaled.add(DrawModel(point));
+  }
+  return pointsListScaled;
+}
+
+getScaleValue(Size windowSize, List<DrawModel> pointsList) {
+  double highestWidth = 0;
+  double highestHeight = 0;
+  for (var point in pointsList) {
+    if (point.offset.dx > highestWidth) {
+      highestWidth = point.offset.dx;
+    }
+    if (point.offset.dy > highestHeight) {
+      highestHeight = point.offset.dy;
+    }
+  }
+  if (highestHeight > highestWidth) {
+    return ((windowSize.height - 10) / highestHeight);
+  } else {
+    return ((windowSize.width - 10) / highestWidth);
+  }
 }
--- lib/src/app/view/home/home.dart
+++ lib/src/app/view/home/home.dart
@@ -10,29 +10,35 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Size size = WidgetsBinding.instance!.window.physicalSize;
-
   List<DrawModel> pointsList = [
     DrawModel(const Offset(0, 0)),
-    DrawModel(const Offset(100, 50)),
-    DrawModel(const Offset(500, 1000))
+    DrawModel(const Offset(50, 50)),
+    DrawModel(const Offset(100, 60)),
+    DrawModel(const Offset(100, 60)),
+    DrawModel(const Offset(100, 90)),
+    DrawModel(const Offset(50, 80)),
+    DrawModel(const Offset(-50, 30))
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("home"),
+          title: const Text("home"),
         ),
         body: Container(
           color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
           // Inner yellow container
-          child: ClipRect(
+          child: Center(
+              child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.grey),
             child: CustomPaint(
               painter: MowerMap(pointsList),
-              size: Size(size.width, size.height),
             ),
-          ),
+          )),
         ));
   }
 }
