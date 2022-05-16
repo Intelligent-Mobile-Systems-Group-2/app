04bac80ab9b1d1e535308f16fbc47fb45e0ceda6
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2022-05-16 15:20:18 +0200

    Dummy data is now converted to different types of lists

--- lib/src/app/view/home/map.dart
+++ lib/src/app/view/home/map.dart
@@ -6,10 +6,18 @@ class MapState extends StatefulWidget {
   _MapState createState() => _MapState();
 }
 
-class _MapState extends State<MapState> {
-  late final List<DrawModel> pointsList = createDrawModelList();
-  List<ListTile> objectList = createObjectList();
+final apiData = [
+  {'time': "2022/08/31:13:37", "x": 0.0, "y": 0.0},
+  {'time': "2022/08/31:13:38", "x": 100.0, "y": 30.0, "object": "shoe"},
+  {'time': "2022/08/31:13:39", "x": 90.0, "y": 100.0},
+  {'time': "2022/08/31:13:40", "x": -80.3, "y": 20.1, "object": "rock"},
+  {'time': "2022/08/31:13:41", "x": 90.0, "y": -30.1}
+];
 
+class _MapState extends State<MapState> {
+  final List<PointModel> points = convertToPoints(apiData);
+  late final List<DrawModel> pointsList = createDrawModelList(points);
+  late final List<ListTile> objectList = createObjectList(points);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,36 +58,46 @@ class _MapState extends State<MapState> {
   }
 }
 
-createDrawModelList() {
-  //TODO should get a list from request and make DrawModelList
-  final List<DrawModel> pointList = [
-    DrawModel(const Offset(0, 0), "Start"),
-    DrawModel(const Offset(50, 50), "Object"),
-    DrawModel(const Offset(100, 60), "Boundary"),
-    DrawModel(const Offset(100, 80), "Boundary"),
-    DrawModel(const Offset(90, 70), "Boundary"),
-    DrawModel(const Offset(50, 80), "Object"),
-    DrawModel(const Offset(-50, 30), "Object"),
-    DrawModel(const Offset(-100, -60), "Boundary")
-  ];
+convertToPoints(List data) {
+  List<PointModel> points = [];
+  for (final e in data) {
+    points.add(PointModel(e["time"], e["x"], e["y"], e["object"]));
+  }
+  return points;
+}
+
+createDrawModelList(List<PointModel> points) {
+  final List<DrawModel> pointList = [];
+  for (final point in points) {
+    if (point.object == null) {
+      pointList.add(DrawModel(Offset(point.x, point.y), "Boundary"));
+    } else {
+      pointList.add(DrawModel(Offset(point.x, point.y), "Object"));
+    }
+  }
   return pointList;
 }
 
-createObjectList() {
-  //TODO should get a list from request and make ObjectList
-  List<ListTile> objectList = [
-    const ListTile(
-        leading: Text("1"),
-        title: Text("Can"),
-        trailing: Text("2022/11/2 - 13:37")),
-    const ListTile(
-        leading: Text("2"),
-        title: Text("Dog"),
-        trailing: Text("2022/11/2 - 13:40")),
-    const ListTile(
-        leading: Text("3"),
-        title: Text("Bottle"),
-        trailing: Text("2022/11/2 - 13:42"))
-  ];
+createObjectList(List<PointModel> points) {
+  final List<ListTile> objectList = [];
+  var index = 1;
+  for (final point in points) {
+    if (point.object != null) {
+      objectList.add(ListTile(
+          leading: Text(index.toString()),
+          title: Text(point.object),
+          trailing: Text(point.time)));
+      index++;
+    }
+  }
   return objectList;
+}
+
+class PointModel {
+  final String time;
+  final double x;
+  final double y;
+  final String object;
+
+  PointModel(this.time, this.x, this.y, this.object);
 }
