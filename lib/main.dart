1ed141f0ad574ecb0be6fd318d84d4180a6c0bce
Erik Granlund <kireerik2008@hotmail.com>
2022-05-19 13:38:06 +0200

    removed code from bluetooth to prepare for merge from current bluetooth branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,12 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get_it/get_it.dart';
 import 'package:int_app/src/app/view/home/joystick.dart';
 import 'package:int_app/src/app/view/http/http.dart';
-import 'src/app/bluetooth/bluetooth_device_manager.dart';
-import 'src/app/bluetooth/interactors/robot_interactor.dart';
-import 'src/app/bluetooth/controllers/robot_controller.dart';
-import 'src/app/bluetooth/bluetooth_discover_manager.dart';
 import 'dart:async';
 
 GetIt getIt = GetIt.instance;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  getIt.registerSingleton(BluetoothDiscoverManager());
-  getIt.registerSingleton(
-      BluetoothDeviceManager<RobotInteractor, RobotController>());
   runApp(MainScreen());
 }
 
